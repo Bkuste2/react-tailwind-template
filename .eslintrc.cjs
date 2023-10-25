a04a002f0ec4d6bc5c67ff01e5b770d994e9ea26
bkuste2 <igorribeiro632@gmail.com>
2023-10-25 11:56:05 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['@rocketseat/eslint-config/react'],
+  ignorePatterns: ['node_modules', 'dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ]
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
