8f41674ca254d14c7de020f76945f67cdf69e6e5
bkuste2 <igorribeiro632@gmail.com>
2023-10-25 14:01:52 -0300

    ➖ build: remove storybook dependency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['@rocketseat/eslint-config/react', 'plugin:storybook/recommended'],
+  extends: ['@rocketseat/eslint-config/react'],
   ignorePatterns: ['node_modules', 'dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
