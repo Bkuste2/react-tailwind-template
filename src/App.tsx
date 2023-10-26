ab119335320957979cbea01a175882aab9417609
bkuste2 <igorribeiro632@gmail.com>
2023-10-26 00:34:08 -0300

    🔧🚧 chore(WIP): add initial settup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export const App: React.FC = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	return (
+		<div className="flex h-full w-full items-center justify-center">
+			<h1 className="text-3xl font-bold">Hello world!</h1>
+		</div>
+	)
 }
