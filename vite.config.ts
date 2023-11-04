ef909152bf03a24de0718df2bb41c36c8dc772f9
bkuste2 <igorribeiro632@gmail.com>
2023-11-04 16:52:35 -0300

    🔧 chore: add aliases to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@pages': resolve(__dirname, './src/pages'),
+			'@components': resolve(__dirname, './src/components'),
+			'@services': resolve(__dirname, './src/services'),
+			'@helpers': resolve(__dirname, './src/helpers'),
+			'@types': resolve(__dirname, './src/types'),
+		},
+	},
 	plugins: [react()],
 })
