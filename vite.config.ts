8cd70b4a1074ed4e07fab9008ce05fbd3efd4d3a
bkuste2 <igorribeiro632@gmail.com>
2023-11-05 01:24:42 -0300

    🔧 chore: add context alias to project

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
 			'@pages': resolve(__dirname, './src/pages'),
 			'@components': resolve(__dirname, './src/components'),
 			'@services': resolve(__dirname, './src/services'),
+			'@contexts': resolve(__dirname, './src/contexts'),
 			'@helpers': resolve(__dirname, './src/helpers'),
 			'@types': resolve(__dirname, './src/types'),
 		},
