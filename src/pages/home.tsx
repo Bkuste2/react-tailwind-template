a12aee715b1607a5e3981cfe7547dd851a19278b
bkuste2 <igorribeiro632@gmail.com>
2023-11-05 04:23:07 -0300

    🔧 chore: implement data fetching with axios

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
+	const getTodo = async () => {
+		return axios
+			.get('https://jsonplaceholder.typicode.com/todos/1')
+			.then((res) => res.data)
+	}
+
 	const { data, isFetched } = useQuery({
 		queryKey: ['getTodo'],
-		queryFn: () => {
-			return fetch('https://jsonplaceholder.typicode.com/todos/1').then(
-				(response) => response.json(),
-			)
-		},
+		queryFn: getTodo,
 	})
 
 	return (
